53baa8c9d8da7374326728bf68408469ac1d6347
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 20:02:17 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+let user = {
+  name: 'John',
+  years: 27
+};
+
+let { name, years: age, isAdmin = false } = user;
+
+console.log(name);
+console.log(age); 
+console.log(isAdmin);
